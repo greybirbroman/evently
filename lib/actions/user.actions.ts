2d01103c7e0d28550d5a77d2efb3dff0ce17b345
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2024-01-12 15:21:35 +0300

    add getUserById

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -20,6 +20,19 @@ export const createUser = async (user: CreateUserParams) => {
   }
 };
 
+export async function getUserById(userId: string) {
+  try {
+    await connectToDatabase();
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error('User not found');
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
   try {
     await connectToDatabase();
