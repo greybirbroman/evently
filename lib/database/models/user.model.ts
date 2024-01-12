4637d47336834675c57ddf4ed259062124370efa
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2024-01-12 12:39:37 +0300

    add UserSchema

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,8 +1,14 @@
-import { Schema } from "mongoose";
+import { Schema, models, model } from 'mongoose';
 
 const UserShema = new Schema({
-    clerkId: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  photo: { type: String, required: true },
+});
 
-    
-})
+const User = models.User || model('User', UserShema);
+
+export default User;
