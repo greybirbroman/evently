705fc46bc951d1d62e7162613d0b6ad6adbdc6c3
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2024-01-12 16:20:33 +0300

    add Events routing / prepare Event Form by shadcn

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+type EventFormProps = {
+  userId: string;
+  type: 'create' | 'update';
+};
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default EventForm;
