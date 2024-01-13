5e86ab4ae619c539e58641cacf7ef733b0b995ab
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2024-01-13 18:50:44 +0300

    feat: async Main Page / get Events from DB

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,16 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import Image from 'next/image';
 import Collection from '@/components/shared/Collection';
+import { getAllEvents } from '@/lib/actions/event.actions';
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: '',
+    category: '',
+    page: 1,
+    limit: 6,
+  });
+
   return (
     <>
       <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
@@ -39,7 +47,15 @@ export default function Home() {
         <div className='w-full flex flex-col gap-5 md:flex-row'>
           Search CategoryFilter
         </div>
-        <Collection />
+        <Collection
+          data={events?.data}
+          emptyTitle='No Events Found'
+          emptyStateSubtext='Come back later'
+          collectionType='All_Events'
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { CreateEventParams } from '@/types';
+import { CreateEventParams, GetAllEventsParams } from '@/types';
 import { handleError } from '../utils';
 import { connectToDatabase } from '../database';
 import User from '../database/models/user.model';
@@ -32,10 +32,14 @@ export const createEvent = async ({
 };
 
 const populateEvent = async (query: any) => {
-    return query
-    .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
-    .populate({ path: 'category', model: Category, select: '_id name'})
-}
+  return query
+    .populate({
+      path: 'organizer',
+      model: User,
+      select: '_id firstName lastName',
+    })
+    .populate({ path: 'category', model: Category, select: '_id name' });
+};
 
 export const getEventById = async (eventId: string) => {
   try {
@@ -44,10 +48,38 @@ export const getEventById = async (eventId: string) => {
     const currentEvent = await populateEvent(Event.findById(eventId));
 
     if (!currentEvent) {
-        throw new Error(`Could not find event`);
-      }
+      throw new Error(`Could not find event`);
+    }
 
     return JSON.parse(JSON.stringify(currentEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getAllEvents = async ({
+  query,
+  limit = 6,
+  page,
+  category,
+}: GetAllEventsParams) => {
+  try {
+    await connectToDatabase();
+
+    const conditions = {};
+
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: 'desc' })
+      .skip(0)
+      .limit(limit);
+
+      const events = await populateEvent(eventsQuery);
+      const eventsCount = await Event.countDocuments(conditions)
+
+      return {
+        data: JSON.parse(JSON.stringify(events)),
+        totalPages: Math.ceil(eventsCount / limit),
+      }
   } catch (error) {
     handleError(error);
   }
