21ce4585c148852e0ecd8fe5be118bd295f6f3b5
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2024-01-15 18:23:58 +0300

    add CategoryFilter component

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -1,11 +1,64 @@
+'use client';
 
+import { useState, useEffect } from 'react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { getAllCategories } from '@/lib/actions/category.actions';
+import { ICategory } from '@/lib/database/models/category.model';
 
 const CategoryFilter = () => {
-	return (
-		<div>
-			
-		</div>
-	)
-}
+  const [categories, setCategories] = useState<ICategory[]>([]);
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-export default CategoryFilter
+	useEffect(() => {
+    const renderCategories = async () => {
+      const categoriesFromServer = await getAllCategories();
+
+      categoriesFromServer &&
+        setCategories(categoriesFromServer as ICategory[]);
+    };
+    renderCategories();
+  }, []);
+
+
+  const onSelectCategory = (category: string) => {
+		let newUrl = '';
+		    if (category && category !== 'All') {
+		      newUrl = formUrlQuery({
+		        params: searchParams.toString().trim(),
+		        key: 'category',
+		        value: category,
+		      });
+		    } else {
+		      newUrl = removeKeysFromQuery({
+		        params: searchParams.toString(),
+		        keysToRemove: ['category'],
+		      });
+		    }
+		    router.push(newUrl, { scroll: false });
+	};
+
+  return (
+    <Select onValueChange={(value: string) => onSelectCategory(value)}>
+      <SelectTrigger className='select-field'>
+        <SelectValue placeholder='Category' />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value='All' className='select-item p-regular-14'>All</SelectItem>
+				{categories.map((category) => (
+					<SelectItem value={category.name} key={category._id}>{category.name}</SelectItem>
+				))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default CategoryFilter;
